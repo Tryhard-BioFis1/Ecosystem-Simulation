becaa4b416f2a486f1873514ae96499c1478bcc1
JoanPascualRibes <157886495+JoanPascualRibes@users.noreply.github.com>
2024-03-22 15:30:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 import time 
-from methods import Blob, Grid
+from methods import Blob, Grid, Grass
 from plots import diversity
 
 # Parameters of simulation
@@ -22,15 +22,15 @@ BACKGR= (255, 255, 250)
 # Tuneable parameters / characteristics of blobs
 metabolism = 0.1
 energy_to_reproduce = 60
-herbogain = 1 
+phytogain = 1 
 maxage = 300 
 gen_var = 0.01 
 
 # Statistics 
 popu_stat = []
 speed_stat = []
-herbo_stat = []
-carno_stat = []
+phyto_stat = []
+phago_stat = []
 vision_stat = []
 offens_stat = []
 time_per_iter_ = []
@@ -66,6 +66,7 @@ else: blobs = [Blob(random.randint(0, SCREEN_WIDTH // CELL_SIZE-1), random.randi
 
 # Initialize needed objects
 grid = Grid(blobs, SCREEN_WIDTH//CELL_SIZE)
+grass = Grass(SCREEN_WIDTH//CELL_SIZE)
 
 # Main loop
 running = True
@@ -167,12 +168,12 @@ while running:
         metabolism -= 0.01
         if metabolism < 0: metabolism=0
 
-    elif keys[pygame.K_h] and keys[pygame.K_UP]: herbogain += 0.002
+    elif keys[pygame.K_h] and keys[pygame.K_UP]: phytogain += 0.002
     elif keys[pygame.K_h] and keys[pygame.K_DOWN]: 
-        herbogain -= 0.01
-        if herbogain < 0: herbogain=0
+        phytogain -= 0.01
+        if phytogain < 0: phytogain=0
 
-    elif keys[pygame.K_g] and keys[pygame.K_UP]: gen_var += 0.002
+    elif keys[pygame.K_g] and keys[pygame.K_UP]: gen_var = np.abs(gen_var+0.002) + 0.001
     elif keys[pygame.K_g] and keys[pygame.K_DOWN]: 
         gen_var -= 0.01
         if gen_var < 0: gen_var=0
@@ -183,14 +184,20 @@ while running:
         for blob in blobs:
             blob.move(grid)
         
-        # Update the grid with the new positions
+        # Update the grid wkith the new positions
         grid.update(blobs)
 
         # Let blobs gain energy form the enviroment 
         for blob in blobs:
-            blob.vital(grid, metabolism, herbogain)
+            blob.vital(grass, grid, metabolism, phytogain)
 
             # Let blobs depredate each other
+        grass.update()  #Let grass update after being eaten
+        grass.grow()   #Let grass grow
+        grass.update()  #Let grass update after growing
+
+
+
         for blob in blobs:
             if blob.energy > 0:
                 neighbours = grid.blobs_at_tile(blob.x, blob.y)
@@ -213,13 +220,13 @@ while running:
         # Display iteration's statistics and Store data to create the final graphics
         popu_stat.append(len(blobs))
         act_speed_lst = [blob.speed for blob in blobs]
-        act_herbo_lst = [blob.herbo for blob in blobs]
-        act_carno_lst = [blob.carno for blob in blobs]
+        act_phyto_lst = [blob.phyto for blob in blobs]
+        act_phago_lst = [blob.phago for blob in blobs]
         act_vision_lst = [blob.vision for blob in blobs]
         act_offens_lst = [blob.offens for blob in blobs]
         speed_stat.append((np.mean(act_speed_lst), np.std(act_speed_lst)))
-        herbo_stat.append((np.mean(act_herbo_lst), np.std(act_herbo_lst)))
-        carno_stat.append((np.mean(act_carno_lst), np.std(act_carno_lst)))
+        phyto_stat.append((np.mean(act_phyto_lst), np.std(act_phyto_lst)))
+        phago_stat.append((np.mean(act_phago_lst), np.std(act_phago_lst)))
         vision_stat.append((np.mean(act_vision_lst), np.std(act_vision_lst)))
         offens_stat.append((np.mean(act_offens_lst), np.std(act_offens_lst)))
         if len(popu_stat)!=1: veloci_stat.append( (popu_stat[-1]-popu_stat[-2])/popu_stat[-1] )
@@ -230,8 +237,8 @@ while running:
         # print(f"Mean energy: {np.mean([blob.energy for blob in blobs])}, ", end='')
         # print(f"Mean age: {np.mean([blob.age for blob in blobs])}, ", end='')
         # print(f"Mean speed: {np.mean(act_speed_lst)},  ", end='')
-        # print(f"Mean herbiborous: {np.mean(act_herbo_lst)}, ", end='')
-        # print(f"Mean carnivorous: {np.mean(act_carno_lst)}, ", end='')
+        # print(f"Mean herbiborous: {np.mean(act_phyto_lst)}, ", end='')
+        # print(f"Mean carnivorous: {np.mean(act_phago_lst)}, ", end='')
         # print(f"Mean vision: {np.mean(act_vision_lst)}, ", end='')
         # print(f"Mean offens: {np.mean(act_offens_lst)}, ", end='')
         # print(f"Conputation time: {time.time()-t_start_iter}, ", end='')
@@ -253,6 +260,8 @@ while running:
 
 pygame.quit()
 
+print(grass.grass)
+
 
 
 # Show final statistics
@@ -267,12 +276,12 @@ ax1 = fig.add_subplot(2,4,2)
 ax1.errorbar(x=[i+1 for i in range(len(speed_stat))], y=[avg_std[0] for avg_std in speed_stat],
               yerr=[avg_std[1] for avg_std in speed_stat], fmt='o', linewidth=1, capsize=5, color='orange', 
               errorevery=max(1,len(popu_stat)//25), label = 'speed' )
-ax1.errorbar(x=[i+1 for i in range(len(herbo_stat))], y=[avg_std[0] for avg_std in herbo_stat],
-              yerr=[avg_std[1] for avg_std in herbo_stat], fmt='o', linewidth=1, capsize=5, color='green', 
-              errorevery=max(1,len(popu_stat)//25), label = 'herbo' )
-ax1.errorbar(x=[i+1 for i in range(len(carno_stat))], y=[avg_std[0] for avg_std in carno_stat],
-              yerr=[avg_std[1] for avg_std in carno_stat], fmt='o', linewidth=1, capsize=5, color='red', 
-              errorevery=max(1,len(popu_stat)//25), label = 'carno' )
+ax1.errorbar(x=[i+1 for i in range(len(phyto_stat))], y=[avg_std[0] for avg_std in phyto_stat],
+              yerr=[avg_std[1] for avg_std in phyto_stat], fmt='o', linewidth=1, capsize=5, color='green', 
+              errorevery=max(1,len(popu_stat)//25), label = 'phyto' )
+ax1.errorbar(x=[i+1 for i in range(len(phago_stat))], y=[avg_std[0] for avg_std in phago_stat],
+              yerr=[avg_std[1] for avg_std in phago_stat], fmt='o', linewidth=1, capsize=5, color='red', 
+              errorevery=max(1,len(popu_stat)//25), label = 'phago' )
 ax1.errorbar(x=[i+1 for i in range(len(vision_stat))], y=[avg_std[0] for avg_std in vision_stat],
               yerr=[avg_std[1] for avg_std in vision_stat], fmt='o', linewidth=1, capsize=5, color='cyan', 
               errorevery=max(1,len(popu_stat)//25), label = 'vision' )
@@ -289,20 +298,20 @@ ax2.set_xlabel("iteration number")
 ax2.set_ylabel("Duration in seg per iteration")
 
 ax3 = fig.add_subplot(2,4,4, projection='3d')
-ax3.scatter([blob.offens for blob in blobs], [blob.defens for blob in blobs], [blob.carno for blob in blobs], c=[blob.herbo for blob in blobs], s=5, alpha=0.5)
+ax3.scatter([blob.offens for blob in blobs], [blob.defens for blob in blobs], [blob.phago for blob in blobs], c=[blob.phyto for blob in blobs], s=5, alpha=0.5)
 # ax3.scatter([blob.fav_meal[4] for blob in blobs], [blob.fav_meal[5] for blob in blobs], [blob.fav_meal[2] for blob in blobs], c='red', s=5, alpha=0.5)
 ax3.set_xlim(0,1)
 ax3.set_ylim(0,1)
 ax3.set_zlim(0,1)
 ax3.set_xlabel("offens")
 ax3.set_ylabel("defens")
-ax3.set_zlabel("carno")
-# ax3.quiver([blob.carno for blob in blobs], [blob.vision for blob in blobs], [blob.speed for blob in blobs], [blob.fav_meal[0] for blob in blobs], [blob.fav_meal[3] for blob in blobs], [blob.fav_meal[2] for blob in blobs], length=0.1, normalize=True)
+ax3.set_zlabel("phago")
+# ax3.quiver([blob.phago for blob in blobs], [blob.vision for blob in blobs], [blob.speed for blob in blobs], [blob.fav_meal[0] for blob in blobs], [blob.fav_meal[3] for blob in blobs], [blob.fav_meal[2] for blob in blobs], length=0.1, normalize=True)
 
 ax4 = fig.add_subplot(2,4,5, projection='3d')
-ax4.plot([avg_std[0] for avg_std in herbo_stat], [avg_std[0] for avg_std in carno_stat], [avg_std[0] for avg_std in offens_stat])
-ax4.set_xlabel("herbo")
-ax4.set_ylabel("carno")
+ax4.plot([avg_std[0] for avg_std in phyto_stat], [avg_std[0] for avg_std in phago_stat], [avg_std[0] for avg_std in offens_stat])
+ax4.set_xlabel("phyto")
+ax4.set_ylabel("phago")
 ax4.set_zlabel("offens")
 
 ax5 = fig.add_subplot(2,4,6)
